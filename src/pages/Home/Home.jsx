0b7db8ec957a1dd5bf586111cd61e5ff0c6ae143
joan logronio <joan.mlogronio@gmail.com>
2022-09-23 14:52:24 +0800

    new commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
             </h3>
             <p>
               We only use the finest ingredients, and we favour organic,
-              sustainable and local procedure. <br />
+              sustainable and local procedure.
               The result is heartfelt goodnesswith quality baked in.
             </p>
             <br />
